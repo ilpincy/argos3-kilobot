c6e14cee21cecaa015548dcc32456ecb686f6bdc
Carlo Pinciroli <cpinciro@ulb.ac.be>
2016-09-01 17:34:53 -0400

    Forced installation prefix to match the ARGoS one.

--- src/cmake/ARGoSBuildChecks.cmake
+++ src/cmake/ARGoSBuildChecks.cmake
@@ -5,6 +5,7 @@ find_package(PkgConfig)
 pkg_check_modules(ARGOS REQUIRED argos3_simulator)
 set(ARGOS_PREFIX ${ARGOS_PREFIX} CACHE INTERNAL "")
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${ARGOS_PREFIX}/share/argos3/cmake)
+set(CMAKE_INSTALL_PREFIX ${ARGOS_PREFIX} CACHE STRING "Install path prefix, prepended onto install directories." FORCE)
 
 #
 # Check whether all the necessary libs have been installed to compile the
