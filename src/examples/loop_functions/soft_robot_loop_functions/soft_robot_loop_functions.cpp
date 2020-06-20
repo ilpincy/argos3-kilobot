7e2b1ba27caa1c2cda1423266483195f356c7949
Carlo Pinciroli <carlo@pinciroli.net>
2020-06-20 18:14:09 -0400

    Added fix for new body mass, rest length as xml param, and correct stiffness

--- src/examples/loop_functions/soft_robot_loop_functions/soft_robot_loop_functions.cpp
+++ src/examples/loop_functions/soft_robot_loop_functions/soft_robot_loop_functions.cpp
@@ -10,7 +10,9 @@
 
 static const std::string KB_CONTROLLER       = "kbc";
 static const std::string PHYSICS_ENGINE      = "dyn2d";
-static const Real        SOFT_KILOBOT_RADIUS = KILOBOT_RADIUS;
+static const Real        SOFT_KILOBOT_RADIUS = 0.02; // 2 cm
+static const Real        SOFT_KILOBOT_MASS   = KILOBOT_MASS + 0.014; // grams
+static const Real        SPRING_MASS         = 0.02; // grams
 
 /****************************************/
 /****************************************/
@@ -23,11 +25,11 @@ void CSoftRobotLoopFunctions::Init(TConfigurationNode& t_tree) {
       /* Parse attributes */
       GetNodeAttribute(t_tree, "robots_center",      m_cRobotsCenter);
       GetNodeAttribute(t_tree, "robots_per_side",    m_unRobotsPerSide);
-      GetNodeAttribute(t_tree, "robots_distance",    m_fRobotsDistance);
+      GetNodeAttribute(t_tree, "spring_rest_length", m_fSpringRestLength);
       GetNodeAttribute(t_tree, "spring_stiffness",   m_fSpringStiffness);
       GetNodeAttribute(t_tree, "spring_damping",     m_fSpringDamping);
       /* Setup the experiment */
-      m_fSpringRestLength = m_fRobotsDistance - 2 * SOFT_KILOBOT_RADIUS;
+      m_fRobotsDistance = m_fSpringRestLength + 2 * SOFT_KILOBOT_RADIUS;
       PlaceRobots();
       AddSprings();
    }
@@ -68,9 +70,7 @@ void CSoftRobotLoopFunctions::PlaceRobots() {
          for(size_t j = 0; j < m_unRobotsPerSide; ++j) {
             /* Make id */
             cKBId.str("");
-            printf("[DEBUG] i = %zu, j = %zu, idx = %zu\n", i, j, (i + m_unRobotsPerSide * j));
-            cKBId << "kb" << (i + m_unRobotsPerSide * j);
-            printf("[DEBUG] cKBId = %s\n", cKBId.str().c_str());
+            cKBId << "kb" << (i + m_unRobotsPerSide * j + 1);
             /* Calculate position
              *
              * The ids grow with positive x and negative y
@@ -83,11 +83,23 @@ void CSoftRobotLoopFunctions::PlaceRobots() {
              */
             cPos.SetX( m_fRobotsDistance * i - fHalfSideLength + m_cRobotsCenter.GetX());
             cPos.SetY(-m_fRobotsDistance * j + fHalfSideLength + m_cRobotsCenter.GetY());
-            printf("[DEBUG] x = %f, y = %f\n", cPos.GetX(), cPos.GetY());
             /* Add robot to space */
             pcKB = new CKilobotEntity(cKBId.str(), KB_CONTROLLER, cPos);
             AddEntity(*pcKB);
             m_vecRobots.push_back(pcKB);
+            /* Fix robot mass and moment to match the larger version */
+            CEmbodiedEntity& cBodyE =
+               pcKB->GetEmbodiedEntity();
+            CDynamics2DKilobotModel& cModel =
+               dynamic_cast<CDynamics2DKilobotModel&>(
+                  cBodyE.GetPhysicsModel(PHYSICS_ENGINE));
+            cpBody* ptBody = cModel.GetBody();
+            cpBodySetMass(ptBody, SOFT_KILOBOT_MASS);
+            cpBodySetMoment(ptBody,
+               cpMomentForCircle(SOFT_KILOBOT_MASS,
+                                 0.0f,
+                                 2 * SOFT_KILOBOT_RADIUS,
+                                 cpvzero));
          }
       }
    }
@@ -125,14 +137,14 @@ void CSoftRobotLoopFunctions::AddSpring(CKilobotEntity* pc_kb1,
                                         CKilobotEntity* pc_kb2,
                                         const CVector2& c_dir) {
    /* Get the embodied entities */
-   CEmbodiedEntity* pcBodyE1 = &pc_kb1->GetEmbodiedEntity();
-   CEmbodiedEntity* pcBodyE2 = &pc_kb2->GetEmbodiedEntity();
+   CEmbodiedEntity& cBodyE1 = pc_kb1->GetEmbodiedEntity();
+   CEmbodiedEntity& cBodyE2 = pc_kb2->GetEmbodiedEntity();
    /* Get the physics models */
-   CDynamics2DKilobotModel* pcModel1 = &dynamic_cast<CDynamics2DKilobotModel&>(pcBodyE1->GetPhysicsModel(PHYSICS_ENGINE));
-   CDynamics2DKilobotModel* pcModel2 = &dynamic_cast<CDynamics2DKilobotModel&>(pcBodyE2->GetPhysicsModel(PHYSICS_ENGINE));
+   CDynamics2DKilobotModel& cModel1 = dynamic_cast<CDynamics2DKilobotModel&>(cBodyE1.GetPhysicsModel(PHYSICS_ENGINE));
+   CDynamics2DKilobotModel& cModel2 = dynamic_cast<CDynamics2DKilobotModel&>(cBodyE2.GetPhysicsModel(PHYSICS_ENGINE));
    /* Get the Chipmunks bodies */
-   cpBody* pcBody1 = pcModel1->GetBody();
-   cpBody* pcBody2 = pcModel2->GetBody();
+   cpBody* ptBody1 = cModel1.GetBody();
+   cpBody* ptBody2 = cModel2.GetBody();
    /*
     * Calculate the anchors (relative to the robot position)
     * anchor1 = dir * radius
@@ -145,7 +157,7 @@ void CSoftRobotLoopFunctions::AddSpring(CKilobotEntity* pc_kb1,
    cpVect tAnchor2 = cpvneg(tOffset);
    /* Add spring between them */
    cpConstraint* ptSpring = cpDampedSpringNew(
-      pcBody1, pcBody2,
+      ptBody1, ptBody2,
       tAnchor1, tAnchor2,
       m_fSpringRestLength,
       m_fSpringStiffness,
